9682c61f43e11c1f025e4d4087d2a6ed0b263941
petrhruza <petr.hruza@hotmail.cz>
2022-04-09 22:30:01 +0200

    Add Enter key listener on search input.

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -29,7 +30,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Iron man");
+    searchMovies('Avengers');
   }, []);
 
   return (
@@ -39,15 +40,20 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search For Movies..."
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={e => e.key === 'Enter' && searchMovies(searchTerm)}
         />
-        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard  movie={movie} />
+            <MovieCard movie={movie} />
           ))}
         </div>
       ) : (
