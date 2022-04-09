7a456eee7f282e2e83b2412ae1ad287b39fdc204
petrhruza <petr.hruza@hotmail.cz>
2022-04-09 21:47:40 +0200

    Modify App.js and add search.svg (SearchIcon)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from "react";
 
+import "./App.css";
+import SearchIcon from "./search.svg";
+
 //5df036fb
 
 const API_URL = "http://www.omdbapi.com?apikey=5df036fb";
+
+const movie1 = {
+  Title: "Iron Man 3",
+  Year: "2013",
+  imdbID: "tt1300854",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -16,7 +28,43 @@ const App = () => {
     searchMovies("Iron man");
   }, []);
 
-  return <h1>Movie DB App</h1>;
+  return (
+    <div className="app">
+      <h1>Movie Database</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search For Movies..."
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
