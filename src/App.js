9c8691a59a8111813dbd0a787e80022f94ea4fb8
petrhruza <petr.hruza@hotmail.cz>
2022-04-09 21:22:20 +0200

    Connect to OMDB API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+//5df036fb
+
+const API_URL = "http://www.omdbapi.com?apikey=5df036fb";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Iron man");
+  }, []);
+
   return <h1>Movie DB App</h1>;
 };
 
